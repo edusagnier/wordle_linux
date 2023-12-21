a64d90c479fe75e2b7d3d10ec6c9fbdf4409e542
Eduard Sagnier <colasagnier@gmail.com>
2023-12-21 01:57:54 +0100

    El installer para distribucion debian acabado

--- linux/installer.sh
+++ linux/installer.sh
@@ -17,19 +17,34 @@ if [ $USERID == 0 ];then
     	if [ -n "$PRETTY_NAME" ]; then
         	echo "Distribución: $PRETTY_NAME"
 			DISTR=`echo $PRETTY_NAME | awk '{print $1}' | tr '[:upper:]' '[:lower:]'`
-			if [[ "$DISTR" == "debian" || "$DISTR" == "ubuntu" || "$DISTR" == "kali" || "$DISTR" == "Deepin" ]];then
+			if [[ "$DISTR" == "debian" || "$DISTR" == "ubuntu" || "$DISTR" == "kali" || "$DISTR" == "deepin" ]];then
+
 				#sudo apt update
 				#sudo apt install default-mysql-server
 				#sudo systemctl start mysql
+
 				cat "$PATH_INGLISH_DB" | mysql
 				cat "$PATH_SPANISH_DB" | mysql
 				
-				echo "Now we are going to inser User & Password for the database, it doesn't need to be very complicated"
-				read -p "Insert a username for the Database user" username
-				read -p "Insert a password for the Database user" password
-				echo username > credentials.txt
-				echo password >> credentials.txt
+				echo "Now we are going to insert User & Password for the database, it doesn't need to be very complicated"
+				read -p "Insert a username for the Database user: " username
+				read -p "Insert a password for the Database user: " password
+
+				echo $username > credentials.txt
+				echo $password >> credentials.txt
 				echo "Saved on credentials.txt"
+
+				echo "CREATE USER '"$username"'@'localhost' IDENTIFIED BY '"$password"';" | mysql
+				echo "MySQL user created" && echo "SELECT user, host FROM mysql.user;" | mysql
+
+				#TEST echo "DROP USER '"$username"'@'localhost'" | mysql
+				#TEST echo MySQL user deleted && echo "SELECT user, host FROM mysql.user;" | mysql
+
+				echo "GRANT ALL PRIVILEGES ON wordle.* TO '"$username"'@'localhost';" | mysql
+				echo "GRANT ALL PRIVILEGES ON wordle_english.* TO '"$username"'@'localhost';" | mysql
+				echo "Privileges on your user given on databases"
+
+
 			fi
     	else
         	echo "No se pudo determinar la distribución."
